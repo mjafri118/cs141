7c7b6c67b02d484b974045afceab6c4c895ff180
kvothey <victor_l_qin@college.harvard.edu>
2019-02-05 09:11:07 -0500

    Problem 1 - begin the real work

--- pa1/lab1_start/main.v
+++ pa1/lab1_start/main.v
@@ -8,7 +8,9 @@ module main(switch, led);
 
 	assign led[7:4] = 4'b0000;
 
-	// Your code here
+	// problem 1
+	assign led[0] = switch[0] & switch[1];
+
 endmodule
 
 `default_nettype none //reengages default behaviour: needed for other Xilinx modules
