1eda29433110c0393b3a2917e46918878ee8fc33
kvothey <victor_l_qin@college.harvard.edu>
2019-02-13 20:04:01 -0500

    Quick push for ALU

--- pa2/lab2_start/alu.v
+++ pa2/lab2_start/alu.v
@@ -25,6 +25,9 @@ module alu(X,Y,Z,op_code, equal, overflow, zero);
 		//functional blocks
 	
    //YOUR CODE HERE - remember to use a separate file for each module you create
+	and_operator AND_op(.X(X), .Y(Y), .Z(and_out));
+	
+	mux_16to1 MUX_16(.A(0), .B(0), .C(0), .D(0), .E(0), .F(0), .G(0), .H(0), .I(0), .J(0), .K(0), .L(0), .M(0), .N(0), .O(0), .P(0), .S(op_code), .Z(Z));
 	//going to have to program a gate level ckt of a mux - see the book for tristates or AND stuffs
 	//Could also be much shorter - very much need to check w TAs
 
