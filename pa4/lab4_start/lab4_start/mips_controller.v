4808bd2df75c9cdfb56016402d2cc6cae9434d68
Mohib Jafri <mjafri@Mohibs-MacBook-Pro.local>
2019-04-05 17:24:39 -0400

    FSM For R Type Implemented

--- pa4/lab4_start/lab4_start/mips_controller.v
+++ pa4/lab4_start/lab4_start/mips_controller.v
@@ -30,32 +30,157 @@ module mips_controller(clk, Funct, OpCode, MemtoReg, RegDST, IorD, PCSrc, ALUSrc
 	// INPUT
 	input wire clk;
 	input wire [5:0] Funct, OpCode;
-	output wire MemtoReg, RegDST, IorD, PCSrc, ALUSrcA, IRWrite, MemWrite, PCWrite, Branch, RegWrite;
+	output reg MemtoReg, RegDST, IorD, PCSrc, ALUSrcA, IRWrite, MemWrite, PCWrite, Branch, RegWrite;
 	reg MemtoReg_next, RegDST_next, IorD_next, PCSrc_next, ALUSrcA_next, IRWrite_next, MemWrite_next, PCWrite_next, Branch_next, RegWrite_next;
-	// @VICTOR: make NEXT regs for all outputs
-	output wire [1:0] ALUSrcB;
+	output reg [1:0] ALUSrcB;
+	reg [1:0] ALUSrcB_next;
 	output wire [3:0] ALUControl;
 	
+	reg [1:0] ALUOp, ALUOp_next;
+
 	
 	// FSM Variables
 	reg [3:0] state, next_state; 
 	
-	wire [1:0] ALUOp;
+	// Instantiate External ALU Decoder
+	alu_decoder aluDecoder(.ALUOp(ALUOp), .Funct(Funct), .ALUControl(ALUControl));
+	
+	
 	// OUTPUT
 	
 		//change to next state and change value of any internal register
-	always @(posedge clk or posedge rst) begin 
-		if (rst) begin
-			state <= `s0; 
-		end
-		else begin
-			state <= next_state;
-			last_green <= last_green_next;
-			//next_ped <= next_ped_next;
-		end
+	always @(posedge clk) begin 
+		//if (rst) begin
+			//state <= `s0; 
+		//end
+
+		state <= next_state;
+		
+		// Next statements for outputs
+		MemtoReg <= MemtoReg_next;
+		RegDST <= RegDST_next;
+		IorD <= IorD_next;
+		PCSrc <= PCSrc_next;
+		ALUSrcA <= ALUSrcA_next;
+		ALUSrcB <= ALUSrcB_next;
+		IRWrite <= IRWrite_next;
+		MemWrite <= MemWrite_next;
+		PCWrite <= PCWrite_next;
+		Branch <= Branch_next;
+		RegWrite <= RegWrite_next;
+			
 	end 
 
+	always @(Funct, OpCode) begin
+		case (state)
+			default : begin	// equivalent to s0
+				// multiplexer selects
+				// DC when doesn't show up in FSM
+				IorD_next <= 0;
+				ALUSrcA_next <= 0;
+				ALUSrcB_next <= 2'b01;
+				ALUOp_next <= 2'b00;
+				PCSrc_next <= 0;
+				
+				// Register Enables
+				// if they don't show up, must be set as 0
+				IRWrite_next <= 1;
+				PCWrite_next <= 1;
+				MemWrite_next <= 0;
+				Branch_next <= 0;
+				RegWrite_next <= 0;
+				
+				next_state <= s0;
+				
+			end
+		
+			s0 : begin // fetch, reset state
+				// multiplexer selects
+				// DC when doesn't show up in FSM
+				ALUSrcA_next <= 0;
+				ALUSrcB_next <= 2'b01;
+				ALUOp_next <= 2'b00;
+				
+				// Register Enables
+				// if they don't show up, must be set as 0
+				IRWrite_next <= 0;
+				PCWrite_next <= 0;
+				MemWrite_next <= 0;
+				Branch_next <= 0;
+				RegWrite_next <= 0;
+				
+				next_state <= s1;
+			
+			end
+			
+			s1 : begin // decode	
+							// Go to R-Type FSM
+				if (OpCode == 6'b000000) begin
+					// multiplexer selects
+					// DC when doesn't show up in FSM
+					ALUSrcA_next <= 1;
+					ALUSrcB_next <= 2'b00;
+					ALUOp_next <= 2'b10;
+					
+					// Register Enables
+					// if they don't show up, must be set as 0
+					IRWrite_next <= 0;
+					PCWrite_next <= 0;
+					MemWrite_next <= 0;
+					Branch_next <= 0;
+					RegWrite_next <= 0;
+					
+				   next_state <= s6;
+				end
+				
+			end
+			
+			s6 : begin // state 6
+				// multiplexer selects
+				// DC when doesn't show up in FSM
+				RegDST_next <= 1;
+				MemtoReg_next <= 0;
+				
+				// Register Enables
+				// if they don't show up, must be set as 0
+				IRWrite_next <= 0;
+				PCWrite_next <= 0;
+				MemWrite_next <= 0;
+				Branch_next <= 0;
+				RegWrite_next <= 1;
+				
+				next_state <= s7;
+			
+			end
+			
+			s7 : begin // state 7
+				// multiplexer selects
+				// DC when doesn't show up in FSM
+				IorD_next <= 0;
+				ALUSrcA_next <= 0;
+				ALUSrcB_next <= 2'b01;
+				ALUOp_next <= 2'b00;
+				PCSrc_next <= 0;
+				
+				// Register Enables
+				// if they don't show up, must be set as 0
+				IRWrite_next <= 1;
+				PCWrite_next <= 1;
+				MemWrite_next <= 0;
+				Branch_next <= 0;
+				RegWrite_next <= 0;
+				
+				next_state <= s0;
+			
+			end
+			
+		endcase
+	
+	end
+
 	//port definitions - customize for different bit widths
+
+
 
 
 endmodule
