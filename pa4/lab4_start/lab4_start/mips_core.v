12ebfd6bf1e17fbc048a635e6ef54334e802e69f
Mohib Jafri <mjafri@Mohibs-MacBook-Pro.local>
2019-04-05 02:46:23 -0400

    MultiCycle Datapath Wired

--- pa4/lab4_start/lab4_start/mips_core.v
+++ pa4/lab4_start/lab4_start/mips_core.v
@@ -11,8 +11,14 @@
 //////////////////////////////////////////////////////////////////////////////////
 module mips_core(
 	clk, 
-	mem_wr_ena, mem_addr, mem_wr_data, mem_rd_data);
+	mem_wr_ena, mem_addr, mem_wr_data, mem_rd_data,
+	PCWrite, IorD, IRWrite, RegDst, MemtoReg, RegWrite, ALUSrcA, ALUSrcB, ALUControl
+	
+	);
 
+	// --------- VARIABLES/WIRES --------
+	
+	
 	//parameter definitions
 	parameter N = 32;
 	parameter I_LENGTH = 1024;
@@ -29,6 +35,15 @@ module mips_core(
 	output wire [N-1:0] mem_addr, mem_wr_data;
 	input wire [N-1:0] mem_rd_data;
 	
+	// INTERNALS
+	reg [N-1:0] PC, PC_0, ALUOut, Data, SignImm, SrcA, SrcB, A, B, Instr, WD3, RD1, RD2;
+	reg [4:0] A3;
+	
+	// ASSIGNS
+	assign mem_wr_data = B;
+	
+	// ---------- INSTANTIATING EXTERNAL MODULES ---------
+	
 	// ----- MUXES -----
 	// naming convention based on control signals.
 	
@@ -36,58 +51,61 @@ module mips_core(
 	two_mux #(
 		.N(32)
 	) IorD_MUX(
-			.X(), .Y(), .Z(), .CTRL(IorD));
+			.X(PC), .Y(ALUOut), .Z(mem_addr), .CTRL(IorD));
 			
 	two_mux #(
 		.N(32)
 	) RegDst_MUX(
-			.X(), .Y(), .Z(), .CTRL(RegDst));
+			.X(Instr[20:16]), .Y(Instr[15:11]), .Z(A3), .CTRL(RegDst));
 			
 	two_mux #(
 		.N(32)
 	) MemtoReg_MUX(
-			.X(), .Y(), .Z(), .CTRL(MemtoReg));
+			.X(ALUOut), .Y(Data), .Z(WD3), .CTRL(MemtoReg));
 			
 	two_mux #(
 		.N(32)
 	) ALUSrcA_MUX(
-			.X(), .Y(), .Z(), .CTRL(ALUSrcA));
+			.X(PC), .Y(A), .Z(SrcA), .CTRL(ALUSrcA));
 			
 	// Four to One's
-	four_mux #(N(32)
+	four_mux #(.N(32)
 	) ALUSrcB_MUX(
-		.A(),.B(),.C(),.D(),.CTRL(ALUSrcB),.Z());
+		.A(B),.B(32'd4),.C(SignImm),.D(),.CTRL(ALUSrcB),.Z(SrcB));
 		
 	// ----- ALU -----
-	alu #(N(32)
-	) ALU(.x(), .y(), .op_code(ALUControl), .z(), .equal(), .zero(), .overflow());
+	alu #(.N(32)
+	) ALU(.x(SrcA), .y(SrcB), .op_code(ALUControl), .z(PC_0), .equal(), .zero(), .overflow());
 	
 	// ----- ARCHITECTURAL MEMORY (i.e. legit just registers) -----
 	// naming convention based on outputs
-	register #(N(32)
-	) PC_AM(.clk(), .rst(), .d(), .q(), .ena(PCWrite));
+	register #(.N(32)
+	) PC_AM(.clk(clk), .rst(), .d(PC_0), .q(PC), .ena(PCWrite));
 	
-	register #(N(32)
-	) Instr_AM(.clk(), .rst(), .d(), .q(), .ena(IRWrite));
+	register #(.N(32)
+	) Instr_AM(.clk(clk), .rst(), .d(mem_rd_data), .q(), .ena(IRWrite));
 	
-	register #(N(32)
-	) Data_AM(.clk(), .rst(), .d(), .q(), .ena(1));
+	register #(.N(32)
+	) Data_AM(.clk(clk), .rst(), .d(mem_rd_data), .q(), .ena(1));
 	
-	register #(N(32)
-	) A_AM(.clk(), .rst(), .d(), .q(), .ena(1));
+	register #(.N(32)
+	) A_AM(.clk(clk), .rst(), .d(RD1), .q(A), .ena(1));
 	
-	register #(N(32)
-	) B_AM(.clk(), .rst(), .d(), .q(), .ena(1));
+	register #(.N(32)
+	) B_AM(.clk(clk), .rst(), .d(RD2), .q(B), .ena(1));
 	
-	register #(N(32)
-	) ALUOut_AM(.clk(), .rst(), .d(), .q(), .ena());
+	register #(.N(32)
+	) ALUOut_AM(.clk(clk), .rst(), .d(PC_0), .q(ALUOut), .ena(1));
 	
 	// ----- REGISTER FILE -----
-	register_file #(N(32)
+	register_file #(.N(32)
 	) Register_File(
-		.clk(), .rst(), 
-		.rd_addr0(), .rd_addr1(), .wr_addr(), .rd_data0(), .rd_data1(), .wr_data(), .wr_ena()); 
-	
+		.clk(clk), .rst(), 
+		.rd_addr0(Instr[25:21]), .rd_addr1(Instr[20:16]), .wr_addr(A3), .rd_data0(RD1), .rd_data1(RD2), .wr_data(WD3), .wr_ena(RegWrite)); 
+		
+	// ----- SIGN EXTENSION ------
+	signExt #(.N(32)
+	) SIGN_EXT(.immed(Instr[15:0]), .out(SignImm));
 
 	//port definitions - customize for different bit widths
 
